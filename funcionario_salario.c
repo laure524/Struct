a5de5bc20e0f7f9a5a89982b4306048d1a0acad4
Lauren_Tavares <laurentavares524@gmail.com>
2026-09-14 20:23:07 -0300

    Add files via upload

---
+++ funcionario_salario.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Funcionario {
+    float numero_inscricao;
+    char nome[50];
+    float salario_normal;
+    float salario_extra;
+    float deducao_inss;
+    float salario_liq;
+} Funcionario;
+
+int main(){
+    printf("Informe seu numero de inscricao:");
+    scanf("%f", &numero_inscricao);
+    printf("\nInforme nome:\n");
+    scanf("%s", &nome);
+    printf("Informe")
+}
+   
